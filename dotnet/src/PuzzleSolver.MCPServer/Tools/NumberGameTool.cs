9235caa1251f15f82528ab2f309a140207b98e66
Stephen Newman <steve@newman.digital>
2025-07-24 08:57:23 +0100

    Add tool capabilities.

--- dotnet/src/PuzzleSolver.MCPServer/Tools/NumberGameTool.cs
+++ dotnet/src/PuzzleSolver.MCPServer/Tools/NumberGameTool.cs
@@ -7,7 +7,8 @@ namespace PuzzleSolver.MCPServer.Tools;
 [McpServerToolType]
 public class NumberGameTool
 {
-    [McpServerTool, Description("Solves a popular numbers game. The game is made up of six digits and the goal is to determine how to manipulate the digits using simple arithmetic operations in order to arrive at the target. The game was made famous on the UK TV Show, Countdown.")]
+    [McpServerTool(Name = "number-game-solver", Destructive = false, Idempotent = true, OpenWorld = false, ReadOnly = true)]
+    [Description("Solves a popular numbers game. The game is made up of six digits and the goal is to determine how to manipulate the digits using simple arithmetic operations in order to arrive at the target. The game was made famous on the UK TV Show, Countdown.")]
     public static string Solve(int[] numbers, int target)
     {
         try
@@ -19,7 +20,7 @@ public class NumberGameTool
         }
         catch
         {
-            return "The game could not be solved.";
+            return "The game could not be solved. Check that it is a valid puzzle.";
         }
     }
 }
